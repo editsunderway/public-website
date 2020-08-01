c31ca65bd7dbcf5bdd8ee524a7c689b4e31d8b02
editsunderway <67233077+editsunderway@users.noreply.github.com>
2020-08-01 15:09:01 -0400

    Update data.js

--- src/mock/data.js
+++ src/mock/data.js
@@ -20,10 +20,7 @@ export const aboutData = {
   img: 'profile.jpg',
   paragraphOne: 'Want your writing to be publish-ready but still sound like you?',
   paragraphTwo: "Whether you're going indie or seeking representation, let your work make the best first impression possible",
-  paragraphThree: '* XXX
-  * yyy
-  * xxxx
-  * xxx',
+  paragraphThree: "* XXX  * yyy  * xxxx  * xxx",
   resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
 };
 
