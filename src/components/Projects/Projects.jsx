29a2adb6055beedbb71d622184faf5e063c75a4d
editsunderway <67233077+editsunderway@users.noreply.github.com>
2020-08-01 19:26:04 -0400

    Update Projects.jsx

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -41,7 +41,7 @@ const Projects = () => {
                     distance="30px"
                   >
                     <div className="project-wrapper__text">
-                      <h3 className="project-wrapper__text-title">{title || 'Light Copyediting'}</h3>
+                      <h3 className="project-wrapper__text-title">{title || 'Light Edit'}</h3>
                       <div>
                         <p>
                           {info ||
