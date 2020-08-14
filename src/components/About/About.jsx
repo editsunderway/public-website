17073a466929700c8b48b3d671942bfd44cbeac1
editsunderway <67233077+editsunderway@users.noreply.github.com>
2020-08-14 00:31:24 -0400

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -62,7 +62,7 @@ const About = () => {
                     <Fade bottom duration={1000} delay={800} distance="30px">
           <div className="contact-wrapper">
             <p className="contact-wrapper__text">
-              {cta || 'Would you like to work with me? Awesome!'}
+              
             </p>
             <a
               target="_blank"
