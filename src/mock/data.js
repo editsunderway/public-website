07d8b797b1785dfc98301b5ca468e3f60dce99a8
editsunderway <67233077+editsunderway@users.noreply.github.com>
2020-08-13 22:51:49 -0400

    Update data.js

--- src/mock/data.js
+++ src/mock/data.js
@@ -17,8 +17,8 @@ export const heroData = {
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'profile.jpg',
-  paragraphOne: 'Want your writing to be publish-ready but still sound like you?',
+  img: 'frontpage-image.jpg',
+  paragraphOne: "You want your story to be published, and your unique voice retained',
   paragraphTwo: "Whether you're going indie or seeking representation, let your work make the best first impression possible. Specializing in personalized copyediting and critique services for the following:",
   paragraphThree: "• Adventure  • Book Club Fiction  • Culture  • General Fiction  •",
   paragraphFour: "• Memoir  • Romance  • Travel  • Women's Fiction  •",
