0f32ff38edea1023968e3872abe16ea10ae1d97c
editsunderway <67233077+editsunderway@users.noreply.github.com>
2020-08-13 22:57:22 -0400

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -30,17 +30,16 @@ const Header = () => {
         </div>
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
           <h1 className="hero-title">
-            {title || 'Hi, my name is'}{' '}
-            <span className="text-color-main">{name || 'Your Name'}</span>
+            <span className="text-color-main">{name || 'name'}</span>
             <br />
-            <i>{subtitle || "I'm the Unknown Developer."}</i>
+            <i>{subtitle || "subtitle"}</i>
           </h1>
         </Fade>
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
           <p className="hero-cta">
             <span className="cta-btn cta-btn--hero">
               <Link to="about" smooth duration={1000}>
-                {cta || 'Know more'}
+                {cta || 'cta'}
               </Link>
             </span>
           </p>
